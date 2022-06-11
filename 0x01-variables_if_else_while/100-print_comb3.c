40fa917408a14184901f979813f7c8071f8993d3
chibeze <ask4eze@gmail.com>
2022-06-11 14:20:45 -0700

    Create print combinations of digits file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int start;
+	int sStart;
+
+
+	for (start = 0; start <= 9; start++)
+	{
+	for (sStart = start + 1; sStart <= 9; sStart++)
+	{
+	putchar(start + '0');
+	putchar(sStart + '0');
+	if (start == 8 && sStart == 9)
+	{
+	putchar('\n');
+	break;
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+}
