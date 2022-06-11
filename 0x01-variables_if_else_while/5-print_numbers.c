f4531c8c5bc081479e0b6107e7b5eb6ede96dd47
chibeze <ask4eze@gmail.com>
2022-06-11 03:42:29 -0700

    Create print numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int start;
+
+	start = 0;
+	while (start < 10)
+	{
+	printf("%d", start);
+	start++;
+	}
+	putchar('\n');
+	return (0);
+}
