0c00243884b971d22fd9bdf49ab4efc2f23cbe82
chibeze <ask4eze@gmail.com>
2022-06-12 14:52:38 -0700

    create print sign function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - checks if a character is upper or lower case
+ * @n: this function takes a parameter to check against
+ *
+ * Return: returns 0 or 1 or -1
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
