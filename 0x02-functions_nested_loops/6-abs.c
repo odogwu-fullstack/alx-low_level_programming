c9c43acbdc5a3357bc86216858b38ac6b2da56c4
chibeze <ask4eze@gmail.com>
2022-06-12 15:28:49 -0700

    Create function absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int_abs - checks if a character is upper or lower case
+ * @n: an integer value
+ *
+ * Return: absolute value of a number 
+ */
+int int_abs(int n)
+{
+if (n < 0)
+return -n;
+else
+return n;
+}
