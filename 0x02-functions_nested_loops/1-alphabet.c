fa333f15280ddd817c702018e73eee3e14c5d683
chibeze <ask4eze@gmail.com>
2022-06-12 13:02:35 -0700

    Create print alphabets with _putchar file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char start = 'a';
+
+while(start <= 'z')
+{
+_putchar(start);
+start++;
+}
+_putchar('\n');
+return (0);
+}
