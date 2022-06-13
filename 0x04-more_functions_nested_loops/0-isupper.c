b6e1b134abf797af9b81eb4572f224c42d266ff6
chibeze <ask4eze@gmail.com>
2022-06-13 08:57:52 -0700

    Updfate

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 or 0 (Success)
+*/
+int _isupper(int c)
+{
+	if (c > 65)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
