0779e608aca91bd666d19935b4c1fba1f5fd2a50
chibeze <ask4eze@gmail.com>
2022-06-12 20:07:30 -0700

    Tired, really tired

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_to_98 - checks if a character is upper or lower case
+ * @n: this function takes a parameter to check against
+ *
+ * Return: Doesnt return shit
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+{
+while (n <= 98)
+printf("%d, ", n);
+n++;
+}
+else
+{
+while (n >= 98)
+printf("%d, ", n);
+n--;
+}
+}
