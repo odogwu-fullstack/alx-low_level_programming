497a51159ab8125865b150c6edd8ec8bca1b171c
chibeze <ask4eze@gmail.com>
2022-06-12 19:42:08 -0700

    Create add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add- checks if a character is upper or lower case
+ * @a: this function takes a parameter to check against
+ * @b: integer two to add
+ *
+ * Return: Doesnt return shit
+ */
+int add(int a, int c)
+{
+return a + b;
+}
