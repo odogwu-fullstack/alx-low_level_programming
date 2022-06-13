d0af6c9850384f4ac365c4be218c5ee226eef2ce
chibeze <ask4eze@gmail.com>
2022-06-13 10:14:21 -0700

    Create sum even fibonacci file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - checks if a character is upper or lower case
+ *
+ * Return: Doesnt return shit
+ */
+int main(void)
+{
+int counter = 0;
+unsigned long int firstDigit = 1;
+unsigned long int lastDigit = 2;
+unsigned long int currentDigit = 2;
+unsigned long int sum = 0;
+while (currentDigit <= 4000000)
+{
+if (currentDigit % 2 == 0)
+sum += currentDigit;
+currentDigit = lastDigit + firstDigit;
+firstDigit = lastDigit;
+lastDigit = currentDigit;
+}
+printf("%lu\n", sum);
+return (0);
+}
