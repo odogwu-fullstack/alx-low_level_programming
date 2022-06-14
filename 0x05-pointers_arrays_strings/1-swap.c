e80133ed5228ce59a2fd354c0234c6d315e2cfb3
chibeze <ask4eze@gmail.com>
2022-06-13 20:37:44 -0700

    Swap two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* swap_int checks if a character is upper or lower case
+* @a: a rubbish integer pointer
+* @b: a rubbish integer pointer
+*
+* Return: Doesnt return shit
+*/
+void swap_int(int *a, int *b)
+{
+int c = *a;
+
+*a = *b;
+*b = c;
+}
