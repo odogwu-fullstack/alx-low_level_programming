a1ac1a318471dc331b273390e8a51e9bea691a18
chibeze <ask4eze@gmail.com>
2022-06-13 22:21:42 -0700

    print reverse function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_rev- checks if a character is upper or lower case
+* @s: a rubbish integer pointer
+*
+* Return: Doesnt return shit
+*/
+void print_rev(char *s)
+{
+long int lastIndex = 0;
+long int i;
+
+while (*(s + lastIndex) != '\0')
+{
+lastIndex++;
+}
+for (i = lastIndex - 1; i >= s; --i)
+{
+putchar(*(s + i));
+}
+putchar('\n');
+}
