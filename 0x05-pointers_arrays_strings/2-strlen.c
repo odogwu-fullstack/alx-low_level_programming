99833eaed9f4ce050dbdaed683a39dcd7eb24a48
chibeze <ask4eze@gmail.com>
2022-06-13 21:25:43 -0700

    Create string length function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen- checks if a character is upper or lower case
+* @s: a rubbish integer pointer
+*
+* Return: Doesnt return shit
+*/
+void _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+*s++
+}
+return (length);
+}
