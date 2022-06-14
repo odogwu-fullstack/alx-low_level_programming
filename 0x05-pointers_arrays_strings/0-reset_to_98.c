64348bff91bf0667fc102c748684ccde0360a5c5
odogwu-fullstack <105443491+odogwu-fullstack@users.noreply.github.com>
2022-06-14 04:24:09 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* reset_to98 - checks if a character is upper or lower case
+* reset_to_98 - checks if a character is upper or lower case
 *
 * Return: Doesnt return shit
 */
-void reset_to98(int *n)
+void reset_to_98(int *n)
 {
 *n= 98;
 }
