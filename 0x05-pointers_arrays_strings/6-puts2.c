da19d5f59b2889364afa7fd5fa50d925c950bc08
chibeze <ask4eze@gmail.com>
2022-06-16 04:44:20 -0700

    Puts 2 created

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* puts2- checks if a character is upper or lower case
+* @str: a rubbish integer pointer
+*
+* Return: Doesnt return shit
+*/
+void puts2(char *str)
+{
+int counter = 0;
+int length = 0;
+char *firstPointer;
+
+firstPointer = str;
+while (*str != '\0')
+{
+length++;
+str++;
+}
+str = firstpointer;
+while (counter < length)
+{
+putchar(*(str + counter));
+counter += 2;
+}
+putchar('\n');
+}
