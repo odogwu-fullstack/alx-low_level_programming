5b306cc97acc927737751117a29db2522a6173e0
chibeze <ask4eze@gmail.com>
2022-06-16 04:02:44 -0700

    Create reverse string file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* rev_string - checks if a character is upper or lower case
+* @s: a rubbish integer pointer
+*
+* Return: Doesnt return shit
+*/
+void rev_string(char *s)
+{
+char *firstPointer = s;
+int len = 0;
+char *lastPointer = s;
+int i;
+char temp;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+lastPointer += length - 1;
+for (i = 0; i < len / 2; i++)
+{
+temp = *lastPointer;
+*lastPointer = *firstPointer;
+*firstPointer = temp;
+firstPointer++;
+lastPointer--;
+}
+}
